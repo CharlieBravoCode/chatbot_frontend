8a698fc739b884e2c80143a33edc406c7949ca44
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2023-03-29 17:18:36 +0200

    Initial Commit

---
+++ chatbot_frontend/chatbot_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbot_frontendApp.swift
+//  chatbot_frontend
+//
+//  Created by Christoph Brauer on 29.03.23.
+//
+
+import SwiftUI
+
+@main
+struct chatbot_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
